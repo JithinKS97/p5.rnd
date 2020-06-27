85bcc5680b336bdf90916a2595f0ea2c58ed5905
jithinks <jithin.ks@batonsystems.com>
2020-06-27 10:32:34 +0530

    simplified

--- dist/p5.rnd.js
+++ dist/p5.rnd.js
@@ -479,7 +479,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33953" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33075" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
